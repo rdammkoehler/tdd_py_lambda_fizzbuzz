84c9f2202f3301cca6e0c89b4440d60f8a161082
Rich Dammkoehler <rpd@noradltd.com>
2020-08-10 13:24:42 -0500

    step five, use named args to clarify which is which, again, not super meaningful but in the future we will move the lambda_function function and then we cannot see the arg order as easily

--- lambda_test.py
+++ lambda_test.py
@@ -3,4 +3,4 @@ def lambda_function(event, context):
 
 
 def test_lambda_function_fizz_buzz_returns_one_given_one():
-    assert lambda_function({'given': 1}, None) == '1'
+    assert lambda_function(event={'given': 1}, context=None) == '1'
