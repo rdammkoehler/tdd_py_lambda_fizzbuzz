2c42dc85926d1e65287ad520631093a52276fbfc
Rich Dammkoehler <rpd@noradltd.com>
2020-08-10 13:29:59 -0500

    step eight, failing test, something more interesting though, change things divisible by 3 into fizz

--- lambda_test.py
+++ lambda_test.py
@@ -8,3 +8,7 @@ def test_lambda_function_fizz_buzz_returns_one_given_one():
 
 def test_lambda_function_fizz_buzz_returns_two_given_two():
     assert lambda_function(event={'given': 2}, context=None) == '2'
+
+
+def test_lambda_function_fizz_buzz_returns_fizz_given_three():
+    assert lambda_function(event={'given': 3}, context=None) == 'fizz'
