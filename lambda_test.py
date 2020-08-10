777fe902937b9bb7084082267fb89b008a41eee3
Rich Dammkoehler <rpd@noradltd.com>
2020-08-10 13:20:02 -0500

    first test, fizz buzz lambda, failing

---
+++ lambda_test.py
@@ -0,0 +1,8 @@
+def test_lambda_function_fizz_buzz_returns_one_given_one():
+    expected ='1'
+    event = { 'given': 1 }
+    context = None
+
+    actual = lambda_function(event, context)
+
+    assert actual == expected
