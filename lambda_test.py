3862c46609d3aadc7f6e68d951d3eac8d01ba77a
Rich Dammkoehler <rpd@noradltd.com>
2020-08-10 13:26:21 -0500

    step six, add another tiny test to cause us to change just a little bit, this is a failing test at this point.

--- lambda_test.py
+++ lambda_test.py
@@ -4,3 +4,7 @@ def lambda_function(event, context):
 
 def test_lambda_function_fizz_buzz_returns_one_given_one():
     assert lambda_function(event={'given': 1}, context=None) == '1'
+
+
+def test_lambda_function_fizz_buzz_returns_two_given_two():
+    assert lambda_function(event={'given': 2}, context=None) == '2'
